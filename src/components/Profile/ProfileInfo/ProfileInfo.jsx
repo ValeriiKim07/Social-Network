0e248202ed5aa0ac81314711e813b49ec80c4497
Valerii Kim <keembeatsprod@gmail.com>
2024-05-20 17:47:16 +0300

    local state 71

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,5 +1,6 @@
 import s from './ProfileInfo.module.css';
 import Preloader from '../../common/Preloader/Preloader';
+import ProfileStatus from './ProfileStatus';
 
 const ProfileInfo = props => {
    if (!props.profile) {
@@ -8,14 +9,15 @@ const ProfileInfo = props => {
    return (
       <div>
          <div>
-            <img
+            {/*<img
                className={s.image}
                src='https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300'
                alt=''
-            />
+            />*/}
          </div>
          <div className={s.descriptionBlock}>
             <img src={props.profile.photos.large} alt='' />
+            <ProfileStatus status={'hello'} />
             <p>{props.profile.fullName}</p>
             <p>{props.profile.aboutMe}</p>
          </div>
