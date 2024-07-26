1c6398afb7961e142230a60a33e48ae563ddb4d5
Valerii Kim <keembeatsprod@gmail.com>
2024-07-26 15:58:24 +0300

    useEffect 85

--- src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
@@ -1,21 +1,25 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 
 const ProfileStatusWithHooks = (props) => {
 
-   let [editMode, setEditMode] = useState(false);
-   let [status, setStatus] = useState();
+    let [editMode, setEditMode] = useState(false);
+    let [status, setStatus] = useState(props.status);
 
-   const activateEditMode = () => {
-      setEditMode(true)
-   }
+    useEffect(() => {
+        setStatus(props.status);
+    }, [props.status]);
 
-   const deactivateEditMode = () => {
-      setEditMode(false)
-      props.updateStatus(status);
-   }
+    const activateEditMode = () => {
+        setEditMode(true);
+    };
+
+    const deactivateEditMode = () => {
+        setEditMode(false);
+        props.updateStatus(status);
+    };
 
     const onStatusChange = e => {
-        setStatus(e.currentTarget.value)
+        setStatus(e.currentTarget.value);
     };
 
     return (
@@ -30,8 +34,8 @@ const ProfileStatusWithHooks = (props) => {
             {editMode &&
                 <div>
                     <input onChange={onStatusChange}
-                        onBlur={deactivateEditMode}
-                        autoFocus={true}
+                           onBlur={deactivateEditMode}
+                           autoFocus={true}
                            value={status}
                     />
                 </div>
